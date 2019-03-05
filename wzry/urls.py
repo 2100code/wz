c30c214b2929156df916960ba7403e796d1a318f
tiger <tiger@tiger.com>
2019-03-05 18:08:32 +0800

    提供英雄列表，查询单个英雄

--- wzry/urls.py
+++ wzry/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^herolist', views.herolist, name='herolist'),
+    url(r'^hero/(?P<nameNum>[0-9]+)/$', views.hero, name='hero'),
 ]
--- wzry/views.py
+++ wzry/views.py
@@ -1,4 +1,38 @@
 from django.http import HttpResponse
+import json
+
+
+def defaultRet(data):
+    return responseData(0, data, '')
+
+
+def responseData(code, data, msg):
+    ret = {'code':code, 'data':data, 'msg':msg}
+    return ret
+
+
+
+def readHerolist():
+    file = open('./wzry/templates/hero_list.json', 'r', encoding='utf-8')
+    ret = json.load(file)
+    file.close()
+    return ret
 
 def index(request):
-    return HttpResponse("wz 数据更新2")
+    return HttpResponse("wz 数据更新3")
+
+def herolist(request):
+    ret = readHerolist();
+    ret = defaultRet(ret)
+    return HttpResponse(json.dumps(ret))
+
+
+def hero(request, nameNum):
+    array = readHerolist();
+    ret = {}
+    for heroObject in array:
+        num = heroObject['ename']
+        if str(num) == nameNum :
+            ret = heroObject
+    ret = defaultRet(ret)
+    return HttpResponse(json.dumps(ret))
