72c67e87ea038da96735cada1b5a8ab4a1d354d6
tiger <tiger@tiger.com>
2019-03-11 18:50:07 +0800

    修改接口

--- wzry/views.py
+++ wzry/views.py
@@ -14,16 +14,33 @@ def responseData(code, data, msg):
 
 
 
+# def readHerolist():
+#     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+#     file_path = BASE_DIR + '/templates/hero_list.json'
+#     # file = open('./wzry/templates/hero_list.json', 'r', encoding='utf-8')
+#     file = open(file_path, 'r', encoding='utf-8')
+#
+#     ret = json.load(file)
+#     file.close()
+#     return ret
+
+
 def readHerolist():
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     file_path = BASE_DIR + '/templates/hero_list.json'
     # file = open('./wzry/templates/hero_list.json', 'r', encoding='utf-8')
     file = open(file_path, 'r', encoding='utf-8')
-
-
-    ret = json.load(file)
+    allArray = json.load(file)
     file.close()
+
+    ret = []
+    for obj in allArray:
+        tech = {'ename', 'cname', 'type'}
+        techDict = {key: value for key, value in obj.items() if key in tech}
+        ret.append(techDict)
+
     return ret
+
 
 def index(request):
     return HttpResponse("wz 数据更新3")
